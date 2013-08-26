ebbc506c1b91459cc20c09919c40c6f2b8ce3bf0
tolo137 <matthewj.egan@hotmail.com>
2013-08-26 12:46:17 +1000

    Wrote the main function for the testSuite

--- src/tests/testSuiteMain.py
+++ src/tests/testSuiteMain.py
@@ -1,8 +1,63 @@
-import bw_Lib
-import testSuite
+from testSuite import *
 
 def main():
     print "--- Running Tests ---"
+
+    print "    Beginning: checkLoginDriver"
+    if checkLoginDriver():
+        print "        checkLogin Passed!"
+    else:
+        print "        checkLogin Failed!"
+    print "\n"
+
+    print "    Beginning: checkValidUserDriver"
+    if checkValidUserDriver():
+        print "        checkValidUser Passed!"
+    else:
+        print "        checkValidUser Failed!"
+    print "\n"
+
+    print "    Beginning: storeNewUserDriver"
+    if storeNewUserDriver():
+        print "        storeNewUser Passed!"
+    else:
+        print "        storeNewUser Failed!"
+    print "\n"
+
+    print "    Beginning: readFileIntoArrayDriver"
+    if readFileIntoArrayDriver():
+        print "        readFileIntoArray Passed!"
+    else:
+        print "        readFileIntoArray Failed!"
+    print "\n"
+
+    print "    Beginning: sattoloShuffleDriver"
+    if sattoloShuffleDriver():
+        print "        sattoloShuffle Passed!"
+    else:
+        print "        sattoloShuffle Failed!"
+    print "\n"
+
+    print "    Beginning: submitScoreDriver"
+    if submitScoreDriver():
+        print "        submitScore Passed!"
+    else:
+        print "        submitScore Failed!"
+    print "\n"
+
+    print "    Beginning: sortAllTimeScoresDriver"
+    if sortAllTimeScoresDriver():
+        print "        sortAllTimeScores Passed!"
+    else:
+        print "        sortAllTimeScores Failed!"
+    print "\n"
+
+    print "    Beginning: sortDailyScoresDriver"
+    if sortDailyScoresDriver():
+        print "        sortDailyScores Passed!"
+    else:
+        print "        sortDailyScores Failed!"
+    print "\n"
 
     print "--- Finished Tests ---"
 
